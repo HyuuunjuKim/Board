25c0f4f6d647eb3079e784f8cfe112110386dd0a
HyuuunjuKim <gus7wn@gmail.com>
2019-07-07 21:06:27 +0900

    pagination 기능 추가

--- app_board/models.py
+++ app_board/models.py
@@ -6,6 +6,7 @@ class Board(models.Model) :
     title = models.CharField(max_length = 200)
     pub_date = models.DateField('date published')
     body = models.TextField()
+    writer = models.CharField(max_length = 100)
 
     def __str__(self) :
         return self.title
--- app_board/views.py
+++ app_board/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Board
 from django.utils import timezone
+from django.core.paginator import Paginator
 
 # Create your views here.
 
 def home(request) :
-    boards = Board.objects #admin의 Board안의 데이터들을 boards라는 변수에 담음
-    return render(request, 'home.html', {'posts': boards})
+    posts = Board.objects #admin의 Board안의 데이터들을 boards라는 변수에 담음
+    post_list = Board.objects.all()
+    paginator = Paginator(post_list, 3)
+    page = request.GET.get('page')
+    page_posts = paginator.get_page(page)
+    return render(request, 'home.html', {'posts': posts, 'page_posts' : page_posts})
 
 def detail(request, post_id) :
     post_detail = get_object_or_404(Board, pk = post_id) #pk는 모델에서 찍어낸 객체 구분자
@@ -20,6 +25,7 @@ def create(request) :
     post.title = request.GET['title']
     post.body = request.GET['body']
     post.pub_date = timezone.datetime.now()
+    post.writer = request.GET['writer']
     post.save()
     return redirect('/board/' + str(post.id)) #redirect는 요청이 오면 저쪽 url로 보내줘
 
@@ -36,5 +42,6 @@ def update(request, post_id) :
     update_post = Board.objects.get(id = post_id)
     update_post.title = request.POST['title']
     update_post.body = request.POST['body']
+    update_post.writer = request.POST['writer'] 
     update_post.save()
     return redirect('home')
